1decf56c8c333c10cb93219db60e243d393832c6
al-develop <al.pankratz@hotmail.de>
2016-11-27 23:36:01 +0100

    worked on Ziplogic: added loading of 7z.dll and some exception handling.

--- ZippingEasy.Logic/ZipLogic.cs
+++ ZippingEasy.Logic/ZipLogic.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SevenZip;
 using ZippingEasy.Common.Utils;
+using log4net;
 
 namespace ZippingEasy.Logic
 {
@@ -13,7 +14,6 @@ namespace ZippingEasy.Logic
     {
         public ZipLogic()
         {
-
         }
 
         public async Task<Result> Zip(string sourcePath, string destinationPath, CompressionLevel compressionLevel)
@@ -23,25 +23,65 @@ namespace ZippingEasy.Logic
                 return new Result("directory does not exist", ResultStatus.Warning);
             }
 
-            SevenZipCompressor compressor = new SevenZipCompressor();
-            compressor.CompressionLevel = compressionLevel;
-            compressor.CompressionMethod = CompressionMethod.Default;
-            compressor.CompressionMode = CompressionMode.Create;
+            // set settings for zipping
+            StringBuilder libraryPathBuilder = new StringBuilder();
+            libraryPathBuilder.Append(AppDomain.CurrentDomain.BaseDirectory);
+            if (Environment.Is64BitOperatingSystem)
+            {
+                libraryPathBuilder.Append("x64\\7z.dll");
+            }
+            else
+            {
+                libraryPathBuilder.Append("x86\\7z.dll");
+            }
 
-            string[] sourceFolderName = sourcePath.Split('\\');
+            if (!File.Exists(libraryPathBuilder.ToString()))
+            {
+                LogManager.GetLogger(typeof(ZipLogic)).Error("7z.dll was not found");
+                return new Result("7z.dll was not found", ResultStatus.Error);
+            }
+            SevenZipCompressor.SetLibraryPath(libraryPathBuilder.ToString());
+            SevenZipCompressor compressor = SetCompressionSettings(compressionLevel);
 
-            compressor.CompressDirectory(sourcePath, $"{sourceFolderName}.zip");
-
-            File.Move($"{sourcePath}\\{sourceFolderName}.zip", $"{destinationPath}\\{sourceFolderName}.zip");
+            // zipping            
+            IList<string> subfolders = this.GetSubfolders(sourcePath).ToList();
+            foreach(var srcFolder in subfolders)
+            {
+                string sourceFolderName = srcFolder.Split('\\').Last();
+                string destFolder = $"{destinationPath}\\{sourceFolderName}.zip";
+                compressor.CompressDirectory(srcFolder, destFolder);
+            }
+            //File.Move($"{sourcePath}\\{sourceFolderName}.zip", $"{destinationPath}\\{sourceFolderName}.zip");
 
             return null;
         }
 
+        private SevenZipCompressor SetCompressionSettings(CompressionLevel compressionLevel)
+        {
+            return new SevenZipCompressor()
+            {                
+                CompressionLevel = compressionLevel,
+                CompressionMethod = CompressionMethod.Default,
+                CompressionMode = CompressionMode.Create
+            };
+        }
+
         public IEnumerable<string> GetSubfolders(string sourcePath)
         {
-            IEnumerable<string> subfolders = Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories)
-                                                      .Where(f => !Directory.EnumerateDirectories(f).Any());
-            return subfolders;
+            try
+            {
+                if (!Directory.Exists(sourcePath))
+                    return new List<string>();
+
+                IEnumerable<string> subfolders = Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories)
+                                                          .Where(f => !Directory.EnumerateDirectories(f).Any());
+                return subfolders;
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetLogger(typeof(ZipLogic)).Warn($"Error on getting subfolders:\n{ex.Message}");
+                return new List<string>();
+            }
         }
     }
 }
